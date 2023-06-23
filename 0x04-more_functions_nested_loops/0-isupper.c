6c9a119913921717f32031e7baa2910733cfe4d7
ayindeme <ayindej1@gmail.com>
2023-06-23 06:35:44 -0700

    check for uppercase alphabet

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - upper case alphabet
+ *
+ * @c: input character
+ *
+ * Return: 0 or 1
+ *
+ */
+int _isupper(int c)
+{
+	if	(c	>=	'A'	&&	c	<=	'Z')
+	{
+		return	(1);
+	}
+	else
+	{
+		return	(0);
+	}
+}
